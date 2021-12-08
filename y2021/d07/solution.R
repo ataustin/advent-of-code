ee84e9e71ef27e28f246af0fb68addf0e18c67bf
Adam Austin <ataustin@gmail.com>
2021-12-07 21:01:20 -0600

    add plotting of costs vs positions

--- y2021/d07/solution.R
+++ y2021/d07/solution.R
@@ -24,6 +24,37 @@ optimization <- function(input, cost_fn) {
 optimization(input, cost1)
 optimization(input, cost2)
 
-# benchmarking -- note esp. difference in memory allocation!
+# benchmarking
 bench::mark(brute_force(input, cost2),
             optimization(input, cost2))
+
+# plotting the cost
+plot_data <- function(input, cost_fn, model) {
+  positions <- min(input):max(input)
+  costs     <- numeric(length(positions))
+  for(i in seq_along(positions)) {
+    costs[i] <- cost_fn(input, positions[i])
+  }
+  out <- data.frame(pos  = positions,
+                    cost = costs)
+  out$cost_norm <- 100 * out$cost / max(out$cost)
+  out$model <- model
+  out
+}
+
+pl1 <- plot_data(input, cost1, "Constant")
+pl2 <- plot_data(input, cost2, "Additive")
+pl  <- rbind(pl1, pl2)
+pl$model <- factor(pl$model,
+                   levels = c("Constant", "Additive"),
+                   ordered = TRUE)
+
+library(ggplot2)
+ggplot(pl, aes(pos, cost_norm, color = model)) +
+  geom_line(size = 1.3) +
+  labs(x = "Final Position",
+       y = "Relative Fuel Cost",
+       title = "Fuel cost as a % of max of each solution",
+       color = "Fuel Burn") +
+  theme_minimal() +
+  scale_color_manual(values = c("dodgerblue", "red"))
