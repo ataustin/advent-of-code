f4614442d71f8ee49fef3e647cf3f925757df59b
Adam Austin <ataustin@gmail.com>
2021-12-08 22:44:04 -0600

    use matrices for cleaner solution

--- y2021/d08/solution.R
+++ y2021/d08/solution.R
@@ -1,41 +1,45 @@
 input <- read.table(text = gsub("\\|", "", readLines("input.txt")))
 
-# helper functions
+# helpers
 str_sort <- function(string) paste(sort(unlist(strsplit(string, ""))), collapse = "")
 
-vgsub <- Vectorize(gsub, "pattern")
+segments <- matrix(c(
+  1, 1, 1, 0, 1, 1, 1,  # 0
+  0, 0, 1, 0, 0, 1, 0,  # 1
+  1, 0, 1, 1, 1, 0, 1,  # 2
+  1, 0, 1, 1, 0, 1, 1,  # 3
+  0, 1, 1, 1, 0, 1, 0,  # 4
+  1, 1, 0, 1, 0, 1, 1,  # 5
+  1, 1, 0, 1, 1, 1, 1,  # 6
+  1, 0, 1, 0, 0, 1, 0,  # 7
+  1, 1, 1, 1, 1, 1, 1,  # 8
+  1, 1, 1, 1, 0, 1, 1), # 9
+  nrow = 7,
+  dimnames = list(letters[1:7], 0:9)
+)
 
-nchar_diff <- function(df, numeral, char_target) {
-  target  <- df$code[is.element(df$numeral, numeral)]
-  pattern <- paste0("[", df$code, "]")
-  nchar(vgsub(pattern, "", target)) == char_target
+key <- rowSums(t(segments) %*% segments)
+
+build_col <- function(string) {
+  col  <- setNames(numeric(7), letters[1:7])
+  ones <- unlist(strsplit(string, ""))
+  col[ones] <- 1
+  col
 }
 
-numeral_select <- function(df, nchar, numeral, target) {
-  df$nchar == nchar & nchar_diff(df, numeral, target)
-}
-
-decode <- function(row) {
-  knowns <- data.frame(nchar = c(2, 4, 3, 7), numeral = c(1, 4, 7, 8))
-  df <- data.frame(code = row[1:10])
-  df$nchar <- nchar(df$code)
-  df <- merge(df, knowns, by = "nchar", all.x = TRUE)
+decode <- function(row, key) {
+  codes <- row[1:10]
+  mat   <- sapply(codes, build_col)
+  colnames(mat) <- sapply(codes, str_sort)
+  lock <- rowSums(t(mat) %*% mat)
   
-  df$numeral[numeral_select(df, 6, 1, 1)] <- 6
-  df$numeral[numeral_select(df, 5, 6, 1)] <- 5
-  df$numeral[numeral_select(df, 5, 1, 0)] <- 3
-  df$numeral[df$nchar == 5 & is.na(df$numeral)] <- 2
-  df$numeral[numeral_select(df, 6, 3, 0)] <- 9
-  df$numeral[df$nchar == 6 & is.na(df$numeral)] <- 0
-  
-  df$code <- sapply(df$code, str_sort)
-  display <- sapply(row[11:14], str_sort)
-
-  df$numeral[match(display, df$code)]
+  digits  <- sapply(row[11:14], str_sort)
+  numbers <- names(key)[match(lock[digits], key)]
+  as.integer(numbers)
 }
 
 # challenge 1
-decoded <- apply(input, 1, decode)  # matrix with decoded row in columns
+decoded <- apply(input, 1, decode, key = key)  # matrix with decoded row in columns
 sum(decoded %in% c(1, 4, 7, 8))
 
 # challenge 2
