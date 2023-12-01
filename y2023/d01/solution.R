62d0d42ff50f720ae27dbdc47922548038001ece
Adam Austin <ataustin@gmail.com>
2023-12-01 13:27:22 -0600

    add 2023 d01

---
+++ y2023/d01/solution.R
@@ -0,0 +1,23 @@
+input <- readLines("input.txt")
+
+decode <- function(input) {
+  nums <- gsub("[a-z]", "", input)
+  digs <- paste0(substr(nums, 1, 1), substr(nums, nchar(nums), nchar(nums)))
+  sum(as.integer(digs))
+}
+
+
+## Part 1
+decode(input)
+
+
+### Part 2
+digs <- c(twone = 21, eightwo = 82, oneight = 18,
+          five = 5, nine = 9, eight = 8, two = 2, three = 3, one = 1,
+          four = 4, six = 6, seven = 7)
+
+for(num in names(digs)) {
+  input <- gsub(num, digs[num], input)
+}
+
+decode(input)
