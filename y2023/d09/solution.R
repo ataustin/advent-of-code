ca3e7c06525c4ab96bb437ac0197a7c9b23dbd28
Adam Austin <ataustin@gmail.com>
2023-12-09 11:48:55 -0600

    just reverse the array, silly

--- y2023/d09/solution.R
+++ y2023/d09/solution.R
@@ -1,25 +1,17 @@
 input <- regmatches(readLines("input.txt"), gregexpr("-?\\d+", readLines("input.txt")))
 input <- lapply(input, as.numeric)
 
-get_prediction <- function(history, back = FALSE) {
-  new <- history[if(back) 1 else length(history)]
+get_prediction <- function(history) {
+  new <- history[length(history)]
   while(!all(history == 0)) {
     history <- diff(history)
-    new     <- c(new, history[if(back) 1 else length(history)])
+    new     <- c(new, history[length(history)])
   }
-  if(back) look_back(rev(new)) else sum(new)
-}
-
-look_back <- function(sequence) {
-  result <- 0
-  for(i in 2:length(sequence)) {
-    result <- sequence[i] - result
-  }
-  result
+  sum(new)
 }
 
 # part 1
 sum(sapply(input, get_prediction))
 
 # part 2
-sum(sapply(input, get_prediction, back = TRUE))
+sum(sapply(input, function(x) get_prediction(rev(x))))
