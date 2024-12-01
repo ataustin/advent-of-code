7684c73af493396222d8da3ac129864657bb0c01
Adam Austin <ataustin@gmail.com>
2024-12-01 00:35:36 -0600

    add 2024 d01

---
+++ y2024/d01/solution.R
@@ -0,0 +1,10 @@
+x <- read.table("input.txt", col.names = c("a", "b"))
+
+# part 1
+sum(abs(sort(x$a) - sort(x$b)))
+
+# part 2
+tab   <- table(x$b[x$b %in% x$a])
+score <- as.data.frame(as.numeric(names(tab)) * tab)
+x     <- merge(x, score, by.x = "a", by.y = "Var1")
+sum(x$Freq)
