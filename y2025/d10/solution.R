7fb0e257f01af5c81acad4c18496e72df7645834
Adam Austin <ataustin@gmail.com>
2025-12-30 12:55:22 -0600

    finish 2025 day 10 -- used package lpSolve

--- y2025/d10/solution.R
+++ y2025/d10/solution.R
@@ -1,11 +1,15 @@
+library(lpSolve)
 x <- readLines("input.txt")
+
 
 # helpers
 extract <- function(string, pattern) {
+  # base version of str_extract
   unlist(regmatches(string, gregexpr(pattern, string)))
 }
 
 make_buttons <- function(schematics, ncol) {
+  # turns schematics into matrix of 0/1, one row per button
   spl <- strsplit(schematics, ",")
   int <- lapply(spl, \(s) as.integer(s) + 1)
   mat <- matrix(0, nrow = length(schematics), ncol = ncol)
@@ -15,35 +19,52 @@ make_buttons <- function(schematics, ncol) {
   mat
 }
 
-vec_to_int <- function(vec, base = 2) {
+bin_to_int <- function(vec, base = 2) {
+  # turns a binary vector into an integer
   strtoi(paste(vec, collapse = ""), base)
 }
 
-press <- function(current, button_ints) {
+flip <- function(current, button_ints) {
+  # flips bits of current integer, using all buttons
   unique(outer(current, button_ints, bitwXor))
 }
 
 
-# part 1
-press_vec <- numeric(length(x))
+# computations
+flip_cts <- jolt_cts <- numeric(length(x))
 
 for(i in seq_along(x)) {
+  # parse input
   machine    <- x[i]
-  target     <- ifelse(extract(machine, "[\\.#]") == "#", 1, 0)
+  lights     <- ifelse(extract(machine, "[\\.#]") == "#", 1, 0)
   schematics <- gsub("\\(|\\)", "", extract(machine, "\\([0-9,]*\\)"))
+  buttons    <- make_buttons(schematics, length(lights))
 
-  buttons <- make_buttons(schematics, length(target))
-  b_ints  <- apply(buttons, 1, vec_to_int)
-  t_int   <- vec_to_int(target)
+  # data for part 1
+  button_int <- apply(buttons, 1, bin_to_int)
+  light_int  <- bin_to_int(lights)
 
-  presses <- 0
-  results <- vec_to_int(rep(0L, ncol(buttons)))  # initial value
-  while(!any(results == t_int)) {
-    results <- press(results, b_ints)
-    presses <- presses + 1
+  flip_ct <- 0
+  results <- bin_to_int(rep(0L, ncol(buttons)))  # initial value
+  while(!any(results == light_int)) {
+    results <- flip(results, button_int)
+    flip_ct <- flip_ct + 1
   }
+  flip_cts[i] <- flip_ct
 
-  press_vec[i] <- presses
+  # data for part 2
+  jolts      <- as.integer(extract(gsub("^.*\\{", "", machine), "[0-9]{1,3}"))
+  t_buttons  <- t(buttons)
+
+  obj <- rep(1, ncol(t_buttons))
+  dir <- rep("=", nrow(t_buttons))
+  res <- lp("min", obj, t_buttons, dir, jolts, int.vec = 1:length(obj))
+  jolt_cts[i] <- res$objval
 }
 
-sum(press_vec)
+
+# part 1
+sum(flip_cts)
+
+# part 2
+sum(jolt_cts)
