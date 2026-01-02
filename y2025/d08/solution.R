24c78efe50487de53f799e01257534ef55d1776c
Adam Austin <ataustin@gmail.com>
2026-01-02 16:37:05 -0600

    update 2025 day 08 with cleaner hclust implementation

--- y2025/d08/solution.R
+++ y2025/d08/solution.R
@@ -1,12 +1,30 @@
 x <- read.csv("input.txt", header = FALSE)
 
-n_closest <- if(nrow(x) < 1000) 10 else 1000  # accommodate test and real data
-
 d <- dist(x)
+h <- hclust(d, method = "single")
+
+# part 1
+# h/t Jonathan Marshall @jmarshallnz.bsky.social for the h arg to simplify part 1
+ct1 <- cutree(h, h = sort(d)[1000])  # cut so that 1000 pairs are connected
+tb1 <- sort(table(ct1), decreasing = TRUE)  # groups boxes together and counts
+prod(tb1[1:3])
+
+# part 2
 m <- as.matrix(d)
 diag(m) <- Inf
 
-m2 <- m  # for part 2
+ct2 <- cutree(h, k = 2)  # cut so that there are only 2 groups left
+ix1 <- which(ct2 > 1)    # this is index of the last remaining join
+ix2 <- which(m[ix1, ] == min(m[ix1, ]))  # and this is the one it's closest to
+prod(x[c(ix1, ix2), 1])  # multiply the x values of the points at those indices
+
+
+# my original answer for part 1 (for posterity)
+# I couldn't figure out what h value to use for cutree in part 1 so I did this instead
+n_closest <- if(nrow(x) < 1000) 10 else 1000  # accommodate test and real data
+
+m <- as.matrix(d)
+diag(m) <- Inf
 
 j <- vector("list", n_closest)
 for(i in seq_along(j)) {
@@ -35,11 +53,3 @@ for(i in 1:n_closest) {
 
 tab <- table(boxes$circuit[boxes$circuit > 0])
 prod(sort(tab, decreasing = TRUE)[1:3])
-
-
-# part 2
-h <- hclust(d, method = "single")
-ct2 <- cutree(h, k = 2)  # cut so that there are only 2 groups left
-ix1 <- which(ct2 > 1)    # this is index of the last remaining join
-ix2 <- which(m2[ix1, ] == min(m2[ix1, ]))  # and this is the one it's closest to
-prod(x[c(ix1, ix2), 1])  # multiply the x values of the points at those indices
