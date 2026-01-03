7162482e6a307ec11277c0f77c5d8afa1a8c3d27
Adam Austin <ataustin@gmail.com>
2026-01-03 00:01:54 -0600

    garbage cleanup, beep beep :truck:

--- y2025/d12/solution.R
+++ y2025/d12/solution.R
@@ -2,12 +2,12 @@ x <- readLines("input.txt")
 
 # region data
 regions      <- grep("x", x, value = TRUE)
-region_dims  <- lapply(strsplit(gsub("\\:.*", "", regions), "x"), as.numeric)
+region_dims  <- lapply(strsplit(gsub(":.*", "", regions), "x"), as.numeric)
 min_capacity <- sapply(region_dims, \(d) prod(floor(d / 3)))
 
-# counts and minimum required areas
-count_required <- lapply(strsplit(gsub("^.*: ", "", regions), " "), as.numeric)
-count_totals   <- sapply(count_required, sum)
+# shape data
+count_each   <- lapply(strsplit(gsub("^.*: ", "", regions), " "), as.numeric)
+count_totals <- sapply(count_each, sum)
 
 # solution
 sum(count_totals <= min_capacity)
